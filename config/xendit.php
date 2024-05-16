41be7060d27467643ad0e55d21222946d0419879
Muhammad Irfan <mrfansi@outlook.com>
2024-05-16 22:37:47 +0700

    First init

--- config/xendit.php
+++ config/xendit.php
@@ -3,4 +3,19 @@
 // config for mrfansi/Xendit
 return [
 
+    /**
+     * The Xendit API is organized around REST. Our API has predictable, resource-oriented URLs, and uses HTTP response codes to indicate API errors. We use built-in HTTP features and HTTP verbs, which are understood by off-the-shelf HTTP clients. JSON is returned by all API responses, including errors.
+     */
+    'base_url' => env('XENDIT_BASE_URL', 'https://api.xendit.com'),
+
+    /**
+     * Secret keys are used to authenticate API requests coming from your servers.
+     */
+    'secret_key' => env('XENDIT_SECRET_KEY', ''),
+
+    /**
+     * Public keys are only used to tokenize card information on the client side.
+     */
+    'public_key' => env('XENDIT_PUBLIC_KEY', ''),
+
 ];
--- src/Xendit.php
+++ src/Xendit.php
@@ -4,4 +4,10 @@ namespace mrfansi\Xendit;
 
 class Xendit
 {
+    public function __construct(
+        public string $baseUrl
+    )
+    {
+        $this->baseUrl = 'https://api.xendit.com';
+    }
 }
